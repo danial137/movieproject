0757bd095bbce8d5693c32bd33da5250e959bcd0
danial137 <danial79fakhrabadi@gmail.com>
2025-09-05 02:22:19 +0330

    add mocie trailer section

---
+++ src/components/TrailersSection.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react';
+import { dummyTrailers } from '../assets/assets'
+import BlurCircle from '../components/BlurCircle'
+import ReactPlayer from 'react-player'
+const TrailersSection = () => {
+
+
+    const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[1]);
+    
+
+  return (
+      <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
+          
+
+          <p className='text-gray-300 font-medium text-lg max-w-[960px] mx-auto'>Trailers</p>
+
+          <div className='relative mt-6'>
+              <BlurCircle top='-100px' right='-100px' />
+              <ReactPlayer
+                  url={currentTrailer.videoUrl}
+                  controls={true}
+                  width="960px"
+                  height="540px"
+                  className='mx-auto max-w-full'
+              />
+
+          </div>
+
+          <div className='group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
+              
+              {dummyTrailers.map((trailer) => (
+                  <div>
+                      <img src={trailer.image} alt="tralier" />
+                  </div>
+              ))}
+              
+          </div>
+
+    </div>
+  )
+}
+
+export default TrailersSection
