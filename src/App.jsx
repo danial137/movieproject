73c6cd77f8783e8d0114bf48a38e85c155944087
danial137 <danial79fakhrabadi@gmail.com>
2025-09-04 21:45:20 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
+import Movie from './pages/Movie'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import {Toaster} from 'react-hot-toast'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Footer from './components/Footer'
+
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+      <Toaster/>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movie />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+      
+    </>
+  )
+}
+
+export default App
