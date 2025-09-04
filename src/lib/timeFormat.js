e4845150ec0fb48baf8dd55a2adb4ae197ca65fd
danial137 <danial79fakhrabadi@gmail.com>
2025-09-05 01:22:01 +0330

    refactor a dummy data and structure

---
+++ src/lib/timeFormat.js
@@ -0,0 +1,8 @@
+const timeFormat = (minutes) => {
+    const hour = Math.floor(minutes / 60)
+    const minutesRemainder = minutes % 60;
+    return `${hour} h ${minutesRemainder} m`
+}
+
+
+export default timeFormat
