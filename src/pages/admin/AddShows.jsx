71fa05d7daf802d87c3d52e45d9dc5302c0c5564
danial137 <danial79fakhrabadi@gmail.com>
2025-09-12 00:44:39 +0330

    finish all front pages and start a backend ... :)

--- src/pages/admin/AddShows.jsx
+++ src/pages/admin/AddShows.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets'
-import { StarIcon, CheckIcon } from 'lucide-react'
+import { StarIcon, CheckIcon, DeleteIcon } from 'lucide-react'
 import Loading from '../../components/Loading'
 import Title from './Title'
 import { kConvert } from '../../lib/kConvert'
@@ -19,9 +19,43 @@ const AddShows = () => {
     setNowPlayingMovies(dummyShowsData)
   }
 
+  const handleDateTimeAdd = () => {
+
+    if (!dateTimeInput) return;
+    const [date, time] = dateTimeInput.split('T');
+    if (!date || !time) return;
+
+    setDateTimeSelection((prev) => {
+      const times = prev[date] || [];
+      if (!times.includes(times)) {
+        return { ...prev, [date]: [...times, time] };
+      }
+      return prev;
+    })
+
+  };
+
+  const handleRemoveTime = (date, time) => {
+    setDateTimeSelection((prev) => {
+      const filteredTime = prev[date].filter((t) => t !== time);
+      if (filteredTime.length === 0) {
+        const { [date]: _, ...rest } = prev;
+        return rest;
+      }
+      return {
+        ...prev,
+        [date]: filteredTime,
+      };
+    });
+  };
+
+
+
   useEffect(() => {
     fetchNowPlayingMovies();
   }, [])
+
+
 
   return nowPlayingMovies.length > 0 ? (
     <>
@@ -66,6 +100,50 @@ const AddShows = () => {
           <input min={0} type="number" value={showPrice} onChange={(e) => setShowPrice(e.target.value)} placeholder='Enter show price' className='outline-none' />
         </div>
       </div>
+
+
+      {/* get date and time  */}
+
+
+      <div className='mt-6'>
+        <label className='block text-sm font-medium mb-2'>
+          Selcet Date and Time
+        </label>
+        <div className='inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg'>
+          <input type="datetime-local" value={dateTimeInput} onChange={(e) => setDateTimeInput(e.target.value)}
+            className='outline-none rounded-md'
+
+          />
+          <button onClick={handleDateTimeAdd} className='bg-primary/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer'>
+            Add Time
+          </button>
+        </div>
+      </div>
+
+      {/* display selecting time  */}
+
+      {Object.keys(dateTimeSelection).length > 0 && (
+        <div className='mt-6'>
+          <h2 className='mb-2'>Select DATE-TIME</h2>
+          <ul className='space-y-3'>
+            {Object.entries(dateTimeSelection).map(([date, times]) => (
+              <li key={date}>
+                <div className='font-medium'>{date}</div>
+                <div className='flex flex-wrap gap-2 mt-1 text-sm'>
+                  {times.map((time) => (
+                    <div key={time} className='border border-primary px-2 py-1 flex items-center rounded'>
+                      <span>{time}</span>
+                      <DeleteIcon onClick={() => handleRemoveTime(date, time)} width={15} className='ml-2 text-red-500 hover:red-700  cursor-pointer' />
+                    </div>
+                  ))}
+                </div>
+
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
 
     </>
   ) : <Loading />
