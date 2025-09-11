c9692abd8f854e58b58382f7fa50cdfac4b5a459
danial137 <danial79fakhrabadi@gmail.com>
2025-09-11 16:16:45 +0330

    fix: replace Object.key with Object.keys in ListShows component
    Corrected the method call to use Object.keys() instead of Object.key()
    to properly count occupied seats and calculate earnings.

--- src/pages/admin/ListBooking.jsx
+++ src/pages/admin/ListBooking.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
 
 const ListBooking = () => {
+
+  const currency = import.meta.env._VITE_CURRENCY || "$"
+
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getAllBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+  };
+
+
+
   return (
     <div>ListBooking</div>
   )
