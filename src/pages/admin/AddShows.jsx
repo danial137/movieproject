531f89654d6a5becb9a6e7f05d691efe4c4db731
danial137 <danial79fakhrabadi@gmail.com>
2025-09-12 00:47:21 +0330

    opps :) add last button

--- src/pages/admin/AddShows.jsx
+++ src/pages/admin/AddShows.jsx
@@ -144,6 +144,9 @@ const AddShows = () => {
         </div>
       )}
 
+      <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>
+        Add Show
+      </button>
 
     </>
   ) : <Loading />
